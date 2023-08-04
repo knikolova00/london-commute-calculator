efcb052ee3bbf3427c27e0ab86f95976c257a2e8
knikolova00 <nikolova.kalina00@gmail.com>
2023-08-04 14:25:36 +0100

    Add error handling

--- distanceCalculator.py
+++ distanceCalculator.py
@@ -9,9 +9,20 @@ api_key = os.getenv('API_KEY')
 
 def commuteCalculator(origin, destination, api_key):
     url = f'https://maps.googleapis.com/maps/api/distancematrix/json?origins={origin}&destinations={destination}&key={api_key}'
-    response = requests.get(url)
+    payload = {}
+    headers = {}
+
+    response = requests.get(url, headers=headers, data=payload)
     data = response.json()
-    return data['rows'][0]['elements'][0]['duration']['text']
+    print(response.text)
+    try:
+        return data['rows'][0]['elements'][0]['duration']['text']
+    except IndexError:
+        print("List index out of range. Check if 'rows' or 'elements' are empty.")
+        return None
+    except KeyError:
+        print("'duration' key not found in the data. Check the API response.")
+        return None
 
 
 def client():
@@ -20,3 +31,14 @@ def client():
     # get api from .env
     result = commuteCalculator(origin, destination, api_key)
     print(result)
+
+# Write result to a txt file
+
+
+def writeToFile(result):
+    with open('result.txt', 'w') as f:
+        f.write(result)
+
+
+if __name__ == '__main__':
+    client()
