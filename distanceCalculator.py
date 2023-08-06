7c72d3267e37d975bf1922725dbf5855f9f8bb93
knikolova00 <nikolova.kalina00@gmail.com>
2023-08-06 16:27:40 +0100

    Change to Bing Distance API instead

--- distanceCalculator.py
+++ distanceCalculator.py
@@ -4,26 +4,35 @@ import os
 
 load_dotenv()
 
-api_key = os.getenv('API_KEY')
+api_key = os.getenv('BING_API_KEY')
 
 
 def commuteCalculator(origin, destination, api_key):
 
     # url = f'https://maps.googleapis.com/maps/api/distancematrix/json?origins={origin}&destinations={destination}&key={api_key}'
     # url = f'https://api.distancematrix.ai/distancematrix?origins={origin}&destinations={destination}&transit_mode=rail&key={api_key}'
-    url = f'https://api.distancematrix.ai/maps/api/distancematrix/json?origins={origin}&destinations={destination}&transit_mode=rail&key={api_key}'
-    payload = {}
-    headers = {}
+    url = 'https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix'
+    querystring = {
+        'origins': f'{origin}',
+        'destinations': f'{destination}',
+        'travelMode': 'transit',
+        'timeType': 'arrival',
+        'dateTime': 'now',
+        'key': f'{api_key}'
+    }
 
     # # Data
     # origin = 'Westminster Abbey,London SW1P 3PA,United Kingdom'
     # destination = 'St John\'s Church,London SW6 1PB, United Kingdom'
 
-    response = requests.get(url, headers=headers, data=payload)
+    response = requests.get(url, params=querystring)
     data = response.json()
     print(response.text)
     try:
-        return data['rows'][0]['elements'][0]['duration']['text']
+        for row in data['rows']:
+            for element in row['elements']:
+                duration = element['duration']['text']
+                return duration
     except IndexError:
         print("List index out of range. Check if 'rows' or 'elements' are empty.")
         return None
@@ -35,7 +44,7 @@ def commuteCalculator(origin, destination, api_key):
 def client():
     origin = input('Where are you?(<street,postcode,country>): ')
     destinations = [
-        'Baker Street Station,London NW1 5LA,United Kingdom']
+        'Baker Street Station,London NW1 5LA,United Kingdom;Turnham Green Station,London W4 1LR,United Kingdom']
     # destination = input('Where do you want to go?: ')
     # get api from .env
     result = commuteCalculator(origin, destinations, api_key)
