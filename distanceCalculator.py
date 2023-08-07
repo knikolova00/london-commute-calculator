ba70ad3eaa82e8329232fced5e5b63e130b6a10d
knikolova00 <nikolova.kalina00@gmail.com>
2023-08-07 20:34:19 +0100

    Change request to TFL

--- distanceCalculator.py
+++ distanceCalculator.py
@@ -26,36 +26,28 @@ def getLatLng(address, api_key):
         return None
 
 
-def commuteCalculator(origin, destination, api_key):
+def commuteCalculator(originCoords, destinationCoords, api_key):
 
-    url = 'https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix'
-    querystring = {
-        'origins': f'{origin}',
-        'destinations': f'{destination}',
-        'travelMode': 'transit',
-        'timeType': 'arrival',
-        'dateTime': 'now',
-        'key': f'{api_key}'
+    base_url = "https://api.tfl.gov.uk/Journey/JourneyResults/"
+    url = f"{base_url}{originCoords[0]},{originCoords[1]}/to/{destinationCoords[0]},{destinationCoords[1]}"
+
+    params = {
+        'mode': 'tube,public-bus,dlr,overground,train,tram,dlr',
+        'app_key': tfl_primary_key
     }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if 'journeys' in data and len(data['journeys']) > 0:
+        journey = data['journeys'][0]
+        return journey['duration']
+    else:
+        return None
 
     # # Data
     # origin = 'Westminster Abbey,London SW1P 3PA,United Kingdom'
     # destination = 'St John\'s Church,London SW6 1PB, United Kingdom'
-
-    response = requests.get(url, params=querystring)
-    data = response.json()
-    print(response.text)
-    try:
-        for row in data['rows']:
-            for element in row['elements']:
-                duration = element['duration']['text']
-                return duration
-    except IndexError:
-        print("List index out of range. Check if 'rows' or 'elements' are empty.")
-        return None
-    except KeyError:
-        print("'duration' key not found in the data. Check the API response.")
-        return None
 
 
 def client():
