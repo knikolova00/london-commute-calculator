1011e61b695b5944968df7e1324228d4c3664f50
knikolova00 <nikolova.kalina00@gmail.com>
2023-08-11 15:06:10 +0100

    Include write to a file

--- distanceCalculator.py
+++ distanceCalculator.py
@@ -31,7 +31,7 @@ def getLatLng(address, api_key):
         return None
 
 
-def commuteCalculator(originCoords, destinationCoords):
+def commuteCalculator(originCoords, destinationCoords, destinationAddress):
 
     base_url = "https://api.tfl.gov.uk/Journey/JourneyResults/"
     url = f"{base_url}{originCoords[0]},{originCoords[1]}/to/{destinationCoords[0]},{destinationCoords[1]}"
@@ -46,31 +46,33 @@ def commuteCalculator(originCoords, destinationCoords):
 
     if 'journeys' in data and len(data['journeys']) > 0:
         journey = data['journeys'][0]
-        print(
-            f"{journey['duration']} minutes to get to {destinations[0]}")
+        # print(
+        #     f"{journey['duration']} minutes to get to {destinationAddress}")
+        writeToFile(
+            f"{journey['duration']} minutes to get to {destinationAddress}")
     else:
         print('Error: No results found')
 
-    # # Data
+    # Test origin
     # origin = 'Westminster Abbey,London SW1P 3PA,United Kingdom'
-    # destination = 'St John\'s Church,London SW6 1PB, United Kingdom'
 
 
 def client():
     origin = input('Where are you?(<street,postcode,country>): ')
-    # Turnham Green Station,London W4 1LR,United Kingdom
     originCoords = getLatLng(origin, api_key)
-    destinationCoords = getLatLng(destinations[0], api_key)
-    commuteCalculator(originCoords, destinationCoords)
+
+    for destination in destinations:
+        destinationCoords = getLatLng(destination, api_key)
+        commuteCalculator(originCoords, destinationCoords, destination)
 
 
 # Write result to a txt file
 
-
 def writeToFile(result):
-    with open('result.txt', 'w') as f:
-        f.write(result)
+    with open('result.txt', 'a') as f:
+        f.write(result + '\n')
 
 
+# Run the script
 if __name__ == '__main__':
     client()
